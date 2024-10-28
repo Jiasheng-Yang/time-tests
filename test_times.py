e2b5a7c327ac59b768450d28a627048d04c5f39f
Jiasheng Yang <jy445@nau.edu>
2024-10-28 12:41:41 +0000

    utilize the pytest.mark.parametrize to simplify the multi tests

--- test_times.py
+++ test_times.py
@@ -1,6 +1,40 @@
 import pytest
 from times import time_range, compute_overlap_time
 
+@pytest.mark.parametrize(
+    "range1, range2, expected",
+    [
+        # test_given_input
+        (
+            time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00", number_of_intervals=1, gap_between_intervals_s=0),
+            time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", number_of_intervals=2, gap_between_intervals_s=60),
+            [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
+        ),
+        # test_time_no_overlap
+        (
+            time_range("2010-01-12 08:00:00", "2010-01-12 09:00:00", number_of_intervals=1, gap_between_intervals_s=0),
+            time_range("2010-01-12 10:00:00", "2010-01-12 11:00:00", number_of_intervals=1, gap_between_intervals_s=0),
+            []
+        ),
+        # test_time_multi_intervals
+        (
+            time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00", number_of_intervals=2, gap_between_intervals_s=60),
+            time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", number_of_intervals=2, gap_between_intervals_s=60),
+            [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
+        ),
+        # test_time_adjacent
+        (
+            time_range("2010-01-12 09:00:00", "2010-01-12 10:00:00", number_of_intervals=1, gap_between_intervals_s=0),
+            time_range("2010-01-12 10:00:00", "2010-01-12 11:00:00", number_of_intervals=1, gap_between_intervals_s=0),
+            []
+        ),
+    ]
+)
+def test_compute_overlap_time(range1, range2, expected):
+    result = compute_overlap_time(range1, range2)
+    assert result == expected, f"Expected: {expected}, but got: {result}"
+
+"""
 def test_given_input():
     large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00", number_of_intervals=1, gap_between_intervals_s=0)
     short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", number_of_intervals=2, gap_between_intervals_s=60)
@@ -31,6 +65,7 @@ def test_time_adjacent():
     result = compute_overlap_time(range1, range2)
     expected = []
     assert result == expected, f"Expected: {expected}, but got: {result}"
+"""
 
 def test_wrong_input():
     start_time = "2010-01-12 08:00:00"
